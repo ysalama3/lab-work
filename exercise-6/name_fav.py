23f87cbd3a9997837e6506ea3055ed03d7017d60
Youstina <ysalama3@gmail.com>
2017-02-15 15:28:41 -0800

    created script name_fav

---
+++ exercise-6/name_fav.py
@@ -0,0 +1,9 @@
+# import in the sys module that contains the function argv
+import sys
+
+# define variables
+name = sys.argv[1]
+fav_fruit = sys.argv[2]
+
+# print a sentence using the variables
+print("Hi. My name is "+ name+ ", I like " + fav_fruit)
