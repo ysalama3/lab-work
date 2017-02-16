6e5905d33b138a8460cc4ee40f41599715646793
Youstina <ysalama3@gmail.com>
2017-02-15 16:42:25 -0800

    python does something script

--- exercise-6/does_something.py
+++ exercise-6/does_something.py
@@ -1,6 +1,7 @@
 #! /bin/bash
 # the next line will create a new file called formatted_eBird_data.csv
-replace_newlines.sh eBird_data.csv
+#cat $1 | tr "\r" "\n" > formatted_$1
+replace_newlines.sh $1
 # the next line will replace all extra commas and will replace the contents of formatted_eBird_data.csv
-sed 's/,\s/ /g' formatted_eBird_data.csv > reformatted_eBird_data.csv
-python does_something.py reformatted_eBird_data.csv
+sed 's/,\s/ /g' formatted_$1 > reformatted_$1
+python does_something.py reformatted_$1
