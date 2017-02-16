29db14251284a70a1e22b092a8299de7b96f17ae
Youstina <ysalama3@gmail.com>
2017-02-15 16:11:25 -0800

    correction in python doessomething script

--- exercise-6/does_something.py
+++ exercise-6/does_something.py
@@ -2,5 +2,5 @@
 # the next line will create a new file called formatted_eBird_data.csv
 replace_newlines.sh eBird_data.csv
 # the next line will replace all extra commas and will replace the contents of formatted_eBird_data.csv
-sed 's/,\s/ /g` formatted_eBird_data.csv > reformatted_eBird_data.csv
+sed 's/,\s/ /g' formatted_eBird_data.csv > reformatted_eBird_data.csv
 python does_something.py reformatted_eBird_data.csv
