000b90638c0f135286c02c8023b7a35268515acd
Youstina <ysalama3@gmail.com>
2017-02-16 11:28:35 -0800

    updates to doessomthing script

--- exercise-6/does_something.py
+++ exercise-6/does_something.py
@@ -1,7 +1,18 @@
 #! /bin/bash
 # the next line will create a new file called formatted_eBird_data.csv
 #cat $1 | tr "\r" "\n" > formatted_$1
-replace_newlines.sh $1
+#replace_newlines.sh $1
 # the next line will replace all extra commas and will replace the contents of formatted_eBird_data.csv
-sed 's/,\s/ /g' formatted_$1 > reformatted_$1
-python does_something.py reformatted_$1
+#sed 's/,\s/ /g' formatted_$1 > reformatted_$1
+#python does_something.py reformatted_$1
+
+nature_birds = {}
+ff = open("/home/eeb177-student/Desktop/eeb-177/homework/formatted.ebird.csv", "r", encoding = "ISO-8859-15")
+ss = ff.readlines()[1:]
+for line in ss:
+    sp = line.split(",")[3]
+    fam = line.split(",")[8]
+    nature_birds[sp]= fam
+ff.close()
+
+print("family of " + sp[0] + " is " + name_birds[species[0]])
